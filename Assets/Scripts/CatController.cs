08efceeccc4c59e9ae234b014db4430d412a7e14
Maxiel <ma172579@knights.ucf.edu>
2022-10-11 13:41:32 -0400

    Animation Commit

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -9,11 +9,13 @@ public class CatController : MonoBehaviour
     public AudioClip musicClipTwo;
 
     public AudioSource musicSource;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -23,22 +25,26 @@ public class CatController : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State", 1);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
     }
 }
